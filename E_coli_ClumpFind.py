b81ec04445462eccf17fbae1a3ff85e6a2818044
Eric Burt <eric.burt@uconn.edu>
2020-06-24 18:34:15 -0400

    implement ClumpFind algorithm on the E Coli genome

---
+++ E_coli_ClumpFind.py
@@ -0,0 +1,25 @@
+#Implement the ClumpFind algorithm on the genome of E Coli
+
+import FrequentWords as fw
+import PatternMatch as pm
+import time
+
+e_coli_genome = 'resources/E_coli.txt'
+
+def clumpFind(k, L, t):
+    startTime = time.perf_counter()
+    with open(e_coli_genome, 'r') as file:
+        genome = file.read()
+        results = []
+        for start in range(len(genome)):
+            print(str(start/len(genome)))
+            cut = genome[start:L]
+            matches = fw.frequentWords(cut, k)
+            for pattern in matches:
+                if(len(pm.patternMatch(pattern, cut)) == t):
+                    if(pattern not in results):
+                        results.append(pattern)
+            L = L + 1
+    endTime = time.perf_counter()
+    print(endTime)
+    return results
